84b2dde95c1de7476ffbe55fa2c02b695726b7a4
EC2 Default User <ec2-user@ip-172-31-41-241.ec2.internal>
2022-09-17 02:22:51 +0000

    Next step

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+#docker build --tag=adsz/flask_app .
+docker build --tag=adsz/flask_app:project-ml-microservice-kubernetes .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it -p 8000:80 adsz/flask_app:project-ml-microservice-kubernetes
